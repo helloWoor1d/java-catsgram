fbfee767214bcc4be0e30ee4b49e40286d546993
mira <pavuuuuuk@yandex.ru>
2025-08-31 23:46:25 +0300

    feat: настройка бд user-profile в user-service

--- user-service/src/main/java/ru/practicum/model/Profile.java
+++ user-service/src/main/java/ru/practicum/model/Profile.java
@@ -1,15 +1,19 @@
 package ru.practicum.model;
 
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+
+import java.time.LocalDateTime;
 
 @Builder
 @NoArgsConstructor
@@ -18,19 +22,25 @@ import lombok.ToString;
 @Setter
 @ToString
 
-//@Entity
+@Entity
+@Table(name = "profiles")
 public class Profile {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.SEQUENCE) //toDo: настроить Sequence
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE) //toDo: настроить Sequence
     private long id;
 
+    @Column(name = "keycloak_id")
     private String keycloakId;
 
     private String email;
 
-    private String username;
+    private String login;
 
     private String bio;
 
+    @Column(name = "avatar")
     private String avatarUrl;
+
+    @Column(name = "created")
+    private LocalDateTime createdAt;
 }
